2e73668e81d437e3f69a5893fcedf6eb70204bd5
Goooble <bivinbheemaiah@gmail.com>
2025-09-25 17:17:50 +0530

    bug: username was visible to non logged in users

--- controller/displayHome.js
+++ controller/displayHome.js
@@ -2,12 +2,10 @@ import { getPosts } from "../db/query.js";
 async function displayHome(req, res) {
   const rows = await getPosts();
   // console.log(rows);
-  if (req.user) {
-    if (!req.user.is_member) {
-      rows.map((item) => {
+  if (!req.user || !req.user.is_member) {
+    rows.map((item) => {
         delete item.username;
       });
-    }
     
   }
 
