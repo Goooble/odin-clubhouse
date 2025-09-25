22bcd0f12821c14d23d1f2433fd71165508fffd3
Goooble <bivinbheemaiah@gmail.com>
2025-09-25 17:07:34 +0530

    feat: validate post

--- route/postRouter.js
+++ route/postRouter.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
 import { createPost, deletePost } from "../db/query.js";
 import isAdmin from "../controller/isAdmin.js"
+import { body, validationResult } from "express-validator";
 const postRouter = Router();
 
-postRouter.post("/put-post", (req, res)=>{
+function validate(req, res, next){
+      const result = validationResult(req);
+      if (!result.isEmpty()) {
+        console.log(result.array());
+        res.redirect("/");
+      } else {
+        next();
+      }
+    }
+
+postRouter.post("/put-post", body("post").rtrim().notEmpty(),validate, (req, res)=>{
     const id = req.user.user_id;
     const post = req.body.post;
     createPost(post, id);
