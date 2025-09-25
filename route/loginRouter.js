cbfb9c091d440177973331f48727b89ade4bacb9
Goooble <bivinbheemaiah@gmail.com>
2025-09-25 17:02:16 +0530

    feat: validate signin

--- route/loginRouter.js
+++ route/loginRouter.js
@@ -1,23 +1,27 @@
-import passport from "../authentication.js"
+import passport from "../authentication.js";
 import { Router } from "express";
 import isLoggedIn from "../controller/isLoggedIn.js";
+import { body, validationResult } from "express-validator";
 
 const loginRouter = Router();
-loginRouter.post("/login", passport.authenticate("local", {
+loginRouter.post(
+  "/login",
+  passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/auth-login/login",
-    failureMessage: true
-}))
+    failureMessage: true,
+  })
+);
 
 loginRouter.get("/login", isLoggedIn);
 
-loginRouter.get("/logout", (req, res, next)=> {
-    req.logout((err)=>{
-        if(err){
-            next(err)
-        }
-    });
-    res.redirect("/")
-})
+loginRouter.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      next(err);
+    }
+  });
+  res.redirect("/");
+});
 
-export default loginRouter
+export default loginRouter;
--- route/signinRouter.js
+++ route/signinRouter.js
@@ -1,10 +1,43 @@
 import signin from "../controller/signin.js";
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
+import pool from "../db/pool.js";
 const signinRouter = Router();
 
-signinRouter.get("/signin",(req, res)=>{
-    res.render("signin");
-})
-signinRouter.post("/signin", signin)
+signinRouter.get("/signin", (req, res) => {
+  res.render("signin");
+});
 
-export default signinRouter
+const signinValidation = () => {
+  return [
+      body("username")
+      .notEmpty().withMessage("Enter a username")
+      .custom(async (value)=>{
+        const {rows} = await pool.query("SELECT username FROM users WHERE username=$1;", [value]);
+        console.log(rows[0])
+        if(rows[0]) throw new Error("Username taken")
+      }).withMessage("Username Taken")
+      
+      .isAlphanumeric('en-US', { ignore: '_' }).withMessage("Only Alphabets, numbers and underscores allowed"),
+
+      body("password").notEmpty().withMessage("Enter Password")
+      .custom((value, { req }) => {
+        return value === req.body.confirmPassword;
+      })
+      .withMessage("Please confirm password")
+    ]
+};
+
+function validate(req, res, next){
+      const result = validationResult(req);
+      if (!result.isEmpty()) {
+        console.log(result.array());
+        res.locals.errormsg = result.array({onlyFirstError:true})[0].msg
+        res.render("signin");
+      } else {
+        next();
+      }
+    }
+signinRouter.post("/signin", signinValidation(), validate, signin);
+
+export default signinRouter;
