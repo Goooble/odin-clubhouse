c80fc5c70bc7e39c20fdf974356302ab979d6d96
Goooble <bivinbheemaiah@gmail.com>
2025-09-25 12:00:48 +0530

    feat: block usernames for non members

--- controller/displayHome.js
+++ controller/displayHome.js
@@ -1,7 +1,11 @@
 import { getPosts } from "../db/query.js";
 async function displayHome(req, res){
     const rows = await getPosts();
-    console.log(rows);
+    // console.log(rows);
+    rows.map((item)=>{
+        delete item.username;
+    })
+    console.log(req.user);
     res.render("home", {posts: rows});
 }
 
